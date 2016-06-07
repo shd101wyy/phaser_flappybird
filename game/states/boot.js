698e1e1c28cca847e1044817b5d2d531c9ad3277
Yiyi Wang <shd101wyy@sina.com>
2016-06-07 21:11:09 +0800

    part1 done

--- game/states/boot.js
+++ game/states/boot.js
@@ -1,17 +1,33 @@
 
-'use strict';
+'use strict'
 
 function Boot() {
 }
 
 Boot.prototype = {
   preload: function() {
-    this.load.image('preloader', 'assets/preloader.gif');
+    this.load.image('preloader', 'assets/preloader.gif')
   },
   create: function() {
-    this.game.input.maxPointers = 1;
-    this.game.state.start('preload');
-  }
-};
+    // If this is not a desktop (so it's a mobile device)
+    if (this.game.device.desktop == false) {
+        // Set the scaling mode to SHOW_ALL to show all the game
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-module.exports = Boot;
+        // Set a minimum and maximum size for the game
+        // Here the minimum is half the game size
+        // And the maximum is the original game size
+        this.game.scale.setMinMax(this.game.width/2, this.game.height/2,
+            this.game.width, this.game.height);
+    }
+
+    // Center the game horizontally and vertically
+    this.game.scale.pageAlignHorizontally = true
+    this.game.scale.pageAlignVertically = true
+
+    this.game.input.maxPointers = 1
+    this.game.state.start('preload')
+  }
+}
+
+module.exports = Boot
