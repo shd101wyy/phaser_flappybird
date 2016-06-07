61205d3a5aa90029389616ecab7804bf7461ada7
Yiyi Wang <shd101wyy@sina.com>
2016-06-07 20:46:30 +0800

    begin to build harder version

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path')
+module.exports = {
+	entry: path.resolve(__dirname, 'game/main.js'),
+	output: {
+		path: path.resolve(__dirname, 'lib/'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			 { test: /\.(js|jsx)$/, include: [path.resolve(__dirname, 'game/')], exclude: [/node_modules/], loader: 'babel' }
+		]
+	}
+}
